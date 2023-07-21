af518b8b97c0f69eb376716efa320618ab27de00
Anuksh189 <anukshaa.it_ioe@bkc.met.edu>
2023-07-21 11:34:06 +0530

    first commit

--- dayone/src/org/tnsif/operator/Increment.java
+++ dayone/src/org/tnsif/operator/Increment.java
@@ -5,8 +5,8 @@ public class Increment {
 	public static void main(String[] args) {
 		int x=12;
 		int y=7;
-		x=++y;
-		y=x++;
+		x=++y; //
+		y=x++; //
 		System.out.println(x);
 		System.out.println(y);
 		
